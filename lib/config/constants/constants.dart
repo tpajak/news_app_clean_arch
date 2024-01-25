28fd9730e8ac10b7ee3ede4f1c3ad059116fee1b
Tomasz Pająk <tomasz@pajak.name>
2024-01-25 18:06:47 +0100

    data: Implementing ArticleRepository and preparing for making API call (retrofit)

---
+++ lib/config/constants/constants.dart
@@ -0,0 +1,4 @@
+const String newsApiBaseUrl = 'https://newsapi.org/v2';
+const String newsApiKey = 'fd2b476db9054702918c8864d01ec9b2';
+
+//https://newsapi.org/v2/top-headlines?country=pl&category=business&apiKey=fd2b476db9054702918c8864d01ec9b2#
