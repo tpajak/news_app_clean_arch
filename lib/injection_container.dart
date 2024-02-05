39da2c4a2d79fc88c93ba0252d4ed9c3b4540594
Tomasz Pająk <tomasz@pajak.name>
2024-02-05 18:11:27 +0100

    chore: filename typo

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:news_app_clean_arch2/features/daily_news/data/data_sources/remote/news_api_service.dart';
-import 'package:news_app_clean_arch2/features/daily_news/data/repository/article_reporistory_impl.dart';
+import 'package:news_app_clean_arch2/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:news_app_clean_arch2/features/daily_news/domain/repository/article_repository.dart';
 import 'package:news_app_clean_arch2/features/daily_news/domain/usecases/get_article.dart';
 import 'package:news_app_clean_arch2/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
