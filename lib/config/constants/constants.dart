34d5c15d6c8c88e39d87448c5d4d38ef2d5accd2
Tomasz Pająk <tomasz@pajak.name>
2024-02-05 15:59:12 +0100

    Implementing article_repository and get_article use case

--- lib/config/constants/constants.dart
+++ lib/config/constants/constants.dart
@@ -1,4 +1,6 @@
 const String newsApiBaseUrl = 'https://newsapi.org/v2';
 const String newsApiKey = 'fd2b476db9054702918c8864d01ec9b2';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
 
 //https://newsapi.org/v2/top-headlines?country=pl&category=business&apiKey=fd2b476db9054702918c8864d01ec9b2#
