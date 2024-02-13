3958caebfdca10bbdcdb679cc8a2136b34012677
Tomasz Pająk <tomasz@pajak.name>
2024-02-13 16:55:27 +0100

    fix: Json data mappings

--- lib/features/daily_news/data/data_sources/remote/news_api_service.g.dart
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.g.dart
@@ -34,26 +34,17 @@ class _NewsApiService implements NewsApiService {
     };
     queryParameters.removeWhere((k, v) => v == null);
     final _headers = <String, dynamic>{};
-    final Map<String, dynamic>? _data = null;
-    final _result = await _dio.fetch<List<dynamic>>(
-        _setStreamType<HttpResponse<List<ArticleModel>>>(Options(
-      method: 'GET',
-      headers: _headers,
-      extra: _extra,
-    )
-            .compose(
-              _dio.options,
-              '/top-headlines',
-              queryParameters: queryParameters,
-              data: _data,
-            )
-            .copyWith(
-                baseUrl: _combineBaseUrls(
-              _dio.options.baseUrl,
-              baseUrl,
-            ))));
-    var value = _result.data!
-        .map((dynamic i) => ArticleModel.fromJson(i as Map<String, dynamic>))
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<HttpResponse<List<ArticleModel>>>(
+            Options(method: 'GET', headers: _headers, extra: _extra)
+                .compose(_dio.options, '/top-headlines',
+                    queryParameters: queryParameters, data: _data)
+                .copyWith(
+                    baseUrl: _combineBaseUrls(_dio.options.baseUrl, baseUrl))));
+    List<ArticleModel> value = _result.data!['articles']
+        .map<ArticleModel>(
+            (dynamic i) => ArticleModel.fromJson(i as Map<String, dynamic>))
         .toList();
     final httpResponse = HttpResponse(value, _result);
     return httpResponse;
